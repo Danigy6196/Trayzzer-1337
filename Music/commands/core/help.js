3c73aeadb6a18a4e9965bfead335bfba596cf0aa
Danigy6196 <81564552+Danigy6196@users.noreply.github.com>
2022-02-14 18:55:51 +0200

    Inital Commit

---
+++ Music/commands/core/help.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'help',
+    aliases: ['h'],
+    showHelp: false,
+    utilisation: '{prefix}help',
+
+    execute(client, message, args) {
+        const embed = new MessageEmbed();
+
+        embed.setColor('RED');
+        embed.setAuthor(client.user.username, client.user.displayAvatarURL({ size: 1024, dynamic: true }));
+
+        const commands = client.commands.filter(x => x.showHelp !== false);
+
+        embed.setDescription('Здравей който пише help този бот е направен от Trayzzer#1337 бота е продава за повече информация се свържи със Trayzzer#1337 ');
+        embed.addField(`Enabled - ${commands.size}`, commands.map(x => `\`${x.name}${x.aliases[0] ? ` (${x.aliases.map(y => y).join(', ')})\`` : '\`'}`).join(' | '));
+
+        embed.setTimestamp();
+        embed.setFooter('Мерси че ползвате моя бот', message.author.avatarURL({ dynamic: true }));
+
+        message.channel.send({ embeds: [embed] });
+    },
+};
